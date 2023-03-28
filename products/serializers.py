4fe2900aa825ed3c5422234e504e263a4cf89350
petr <pnikono24@gmail.com>
2023-03-28 11:29:13 +0300

    Update

--- products/serializers.py
+++ products/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 
-from products.models import Product
+from products.models import Product, ProductCategory, Cart
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+    category = serializers.SlugRelatedField(slug_field='name', queryset=ProductCategory.objects.all())
 
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ('id', 'product', 'quantity', 'created_timestamp')
+        read_only_fields = ('created_timestamp',)
